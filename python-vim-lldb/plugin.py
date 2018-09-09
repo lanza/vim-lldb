a43884c84ac61380fab615ea24e309676be84e14
Nathan Lanza <nathan@lanza.io>
2018-09-09 03:11:46 -0700

    Reformat the project with Black

--- python-vim-lldb/plugin.py
+++ python-vim-lldb/plugin.py
@@ -4,11 +4,16 @@
 import import_lldb
 
 try:
-  import lldb
-  import vim
+    import lldb
+    import vim
 except ImportError:
-  sys.stderr.write("Unable to load vim/lldb module. Check lldb is on the path is available (or LLDB is set) and that script is invoked inside Vim with :pyfile")
-  pass
+    sys.stderr.write(
+        """
+        Unable to load vim/lldb module. Check lldb is on the path is available
+        (or LLDB is set) and that script is invoked inside Vim with :pyfile
+        """
+    )
+    pass
 else:
-  # Everthing went well, so use import to start the plugin controller 
-  from lldb_controller import *
+    # Everthing went well, so use import to start the plugin controller
+    from lldb_controller import *
